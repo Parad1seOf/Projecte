875b82fd577be26420f0c8dd09fcf14e5b7ea0c5
Arnau Seguí Borrell <165802178+Parad1seOf@users.noreply.github.com>
2024-05-20 16:13:52 +0200

    Changes on character life

--- Assets/Script#/CharacterLive.cs
+++ Assets/Script#/CharacterLive.cs
@@ -1,5 +1,5 @@
-using System.Collections;
-using System.Collections.Generic;
+// using System.Collections;
+// using System.Collections.Generic;
 using UnityEngine;
 
 public class CharacterLive : MonoBehaviour
@@ -9,17 +9,24 @@ public class CharacterLive : MonoBehaviour
     private void Start()
     {
         HealthManager = new GM_Health();
+        Debug.Log("Initial Health: " + HealthManager.GetHealth());
     }
 
     public void TakeDamage(int damage)
     {
+        Debug.Log("Taking damage: " + damage);
         HealthManager.TakeDamage(damage);
+        Debug.Log("Current Health: " + HealthManager.GetHealth());
 
+        if (HealthManager.GetHealth() <= 0)
+        {
+            Die();
+        }
     }
 
     public void Die()
     {
-
+        Debug.Log("Player is dead");
         HealthManager.Die();
         Destroy(gameObject);
     }
--- Assets/Script#/GM_Health.cs
+++ Assets/Script#/GM_Health.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 public class GM_Health
 {
     [SerializeField] private int Health;
+
     public int GetHealth() => Health;
     public void SetHealth(int health) => Health = health;
     public void TakeDamage(int AttackDamage) => Health -= AttackDamage;
