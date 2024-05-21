4da2ffe60eda8e5046ad11fc55ccf215f479678b
Parad1seOf <aseguib@edu.tecnocampus.cat>
2024-05-21 18:08:39 +0200

    Add Pause GM

--- Assets/Script#/PauseGame.cs
+++ Assets/Script#/PauseGame.cs
@@ -2,45 +2,89 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 public class PauseGame : MonoBehaviour
 {
-    private enum GMStates 
+    private enum EGameState 
     {
         READY,
         PLAYING,
-        PAUSED
+        PAUSED,
+        GAME_OVER
     }
 
-    private GMStates GMState = GMStates.READY;
+    public string _playerTag = "Player";
+    public string _startInput = "ActionInput";
+    public string _pauseInput = "PauseInput";
+    public float _resetGameWaitTime = 0.5f;
 
-    private void Update()
+    private EGameState _currentGameState = EGameState.READY;
+
+    void Start()
     {
-        if (Input.GetKeyDown(KeyCode.Escape))
+        EnterCurrentState();
+    }
+
+    void Update()
+    {
+        if (Input.GetButtonDown(_startInput) && (_currentGameState == EGameState.READY || _currentGameState == EGameState.PAUSED))
+            ChangeGameState(EGameState.PLAYING);
+
+        else if(Input.GetButtonDown(_pauseInput) && _currentGameState == EGameState.PLAYING)
+            ChangeGameState(EGameState.PAUSED);
+    }
+
+    private void OnPlayerDied()
+    {
+        ChangeGameState(EGameState.GAME_OVER);
+    }
+
+    private void ChangeGameState(EGameState newState)
+    {
+        if (newState == _currentGameState)
+            return;
+        Debug.Log(newState.ToString());
+        ExitCurrentState();
+        _currentGameState = newState;
+        EnterCurrentState();
+    }
+
+    private void ExitCurrentState()
+    {
+        switch(_currentGameState) 
         {
-            if (GMState == GMStates.PLAYING)
-            {
-                GMState = GMStates.PAUSED;
-                Time.timeScale = 0;
-            }
-            else if (GMState == GMStates.PAUSED)
-            {
-                GMState = GMStates.PLAYING;
-                Time.timeScale = 1;
-            }
+            case EGameState.READY:
+                Time.timeScale = 1.0f;
+                break;
+            case EGameState.PLAYING:
+                break;
+            case EGameState.PAUSED:
+                Time.timeScale = 1.0f;
+                break;
+            case EGameState.GAME_OVER:
+                Time.timeScale = 1.0f;
+                break;
         }
     }
 
-    public void LoadScene(string sceneName)
+    private void EnterCurrentState()
     {
-        SceneManager.LoadScene(sceneName);
+        switch (_currentGameState)
+        {
+            case EGameState.READY:
+                Time.timeScale = 0.0f;
+                break;
+            case EGameState.PLAYING:
+                break;
+            case EGameState.PAUSED:
+                Time.timeScale = 0.0f;
+                break;
+            case EGameState.GAME_OVER:
+                Time.timeScale = 0.25f;
+                Invoke("ResetGame", _resetGameWaitTime);
+                break;
+        }
     }
 
-    public void QuitGame()
+    private void ResetGame()
     {
-        Application.Quit();
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
-
-    public void RestartGame()
-    {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
-    }
-
 }
