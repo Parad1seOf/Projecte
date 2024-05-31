5bb68a04446280d4dcbc10e3f66b422276f439db
jooaaco <jojedap@edu.tecnocampus.cat>
2024-05-31 20:13:02 +0200

    Pruebas codigo sonido

--- Assets/Script#/ColletableItem.cs
+++ Assets/Script#/ColletableItem.cs
@@ -5,16 +5,25 @@ using UnityEngine;
 public class ColletableItem : MonoBehaviour
 {
     public GM_Item thisobject;
+    
+    [SerializeField] private AudioClip colectar1;
+
+
 
     private void Awake() {
         thisobject = GetComponent<GM_Item>();    
     }
 
     private void OnTriggerEnter2D(Collider2D collision) {
+        
         if (collision.CompareTag("Player")) {
             // Debug.Log("Item Collected");
             Destroy(gameObject);
             GM_Inventori.instance.AddItem(thisobject);
+            
+            ControladorSonido.Instance.EjecutarSonido(colectar1);
         }
+
+     
     }
 }
--- Assets/Script#/EfectodeSonido.cs
+++ Assets/Script#/EfectodeSonido.cs
@@ -2,21 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class EfectodeSonido : MonoBehaviour
 {
-    private AudioSource audioSource;
-
-
-    private void Start()
-    {
-        audioSource = GetComponent<AudioSource>();
-    }
+    [SerializeField] private AudioClip colectar1;
 
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
         {
-            audioSource.Play();
+            ControladorSonido.Instance.EjecutarSonido(colectar1);
         }
     }
 }
