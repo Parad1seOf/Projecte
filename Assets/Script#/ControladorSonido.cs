070bce5eaf93ada89a6e4794caf327f8df372928
jooaaco <jojedap@edu.tecnocampus.cat>
2024-05-31 20:21:05 +0200

    Cambios Sonido

--- Assets/Script#/ControladorSonido.cs
+++ Assets/Script#/ControladorSonido.cs
@@ -4,15 +4,26 @@ using UnityEngine;
 
 public class ControladorSonido : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public static ControladorSonido Instance;
+    private AudioSource audioSource;
+
+    private void Awake()
     {
-        
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+
+        audioSource = GetComponent<AudioSource>();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void EjecutarSonido (AudioClip sonido)
     {
-        
+        audioSource.PlayOneShot(sonido);
     }
 }
