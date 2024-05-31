fac408460c825c6d8ca95327db2d83c184eb88fc
Parad1seOf <aseguib@edu.tecnocampus.cat>
2024-05-31 20:23:40 +0200

    Change on collctable items

--- Assets/Script#/ColletableItem.cs
+++ Assets/Script#/ColletableItem.cs
@@ -8,22 +8,35 @@ public class ColletableItem : MonoBehaviour
     
     [SerializeField] private AudioClip colectar1;
 
-
-
     private void Awake() {
         thisobject = GetComponent<GM_Item>();    
     }
 
-    private void OnTriggerEnter2D(Collider2D collision) {
-        
-        if (collision.CompareTag("Player")) {
-            // Debug.Log("Item Collected");
-            Destroy(gameObject);
-            GM_Inventori.instance.AddItem(thisobject);
-            
-            ControladorSonido.Instance.EjecutarSonido(colectar1);
-        }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            Debug.Log("Player collided with collectible");
 
-     
+            Destroy(gameObject);
+
+            if (GM_Inventori.instance != null)
+            {
+                GM_Inventori.instance.AddItem(thisobject);
+            }
+            else
+            {
+                Debug.LogError("GM_Inventori instance is null");
+            }
+
+            if (ControladorSonido.Instance != null)
+            {
+                ControladorSonido.Instance.EjecutarSonido(colectar1);
+            }
+            else
+            {
+                Debug.LogError("ControladorSonido instance is null");
+            }
+        }
     }
 }
